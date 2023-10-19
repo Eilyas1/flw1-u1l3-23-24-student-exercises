9232e8145c6fcc4afa00b248503220757f1bca4d
Eilyas1 <122292670+Eilyas1@users.noreply.github.com>
2023-10-19 23:26:57 +0000

    FellowShip October 19

--- operators.js
+++ operators.js
@@ -5,18 +5,23 @@
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
-
+let num1 = 9;
+let num2 = 3;
+let sum = num1 + num2
+  console.log(sum)
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
+let difference = num1 - num2
+  console.log(difference)
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
+let product = num1 * num2
+  console.log(product)
 
 
 // String Operations
@@ -26,3 +31,7 @@
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
 
+let animal = "cat";
+let num3 = 8;
+let combinedString = animal + num3
+  console.log(combinedString)
